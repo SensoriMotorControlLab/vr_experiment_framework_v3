fc1c87ca8c900f04e5fb6e0d680c46f563fedc8b
Matthew Bodenstein <matthew_bodenstein@hotmail.com>
2024-08-02 16:31:26 -0400

    updated force applied

--- Assets/Scripts/Tasks/CurrentForce.cs
+++ Assets/Scripts/Tasks/CurrentForce.cs
@@ -18,12 +18,31 @@ public class CurrentForce : MonoBehaviour
         
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Tool"){
-            other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward*forwardForce*1000);
-            other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left*sideForce*1000);
+    // void OnTriggerStay(Collider other)
+    // {
+    //     if (other.gameObject.tag == "Tool"){
+    //         other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward*forwardForce*1);
+    //         other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left*sideForce*1);
+            
 
+    //     }
+    // }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Tool")
+        {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                // Set constant forward velocity
+                Vector3 velocity = rb.velocity;
+                velocity.z = forwardForce;
+                rb.velocity = velocity;
+
+                // Apply side force
+                rb.AddForce(Vector3.left * sideForce);
+            }
         }
     }
 }
